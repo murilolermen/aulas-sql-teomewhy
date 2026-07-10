473a36b217f06d6c742a9ff1f1394afa68671217
Murilo <murilobolis@gmail.com>
2026-07-09 21:23:19 -0300

    aula 16 group by

---
+++ aulas_codigo/12_group_by.sql
@@ -0,0 +1,22 @@
+-- SELECT IdProduto,
+--         count(*)
+
+-- FROM transacao_produto
+
+-- GROUP BY IdProduto
+
+
+SELECT idCliente,
+        sum(qtdePontos) AS somaPontos,
+        count(IdTransacao)
+
+
+FROM transacoes
+
+
+WHERE DtCriacao >= '2025-07-01'
+AND DtCriacao < '2025-08-01'
+
+GROUP BY idCliente
+
+ORDER BY somaPontos DESC
